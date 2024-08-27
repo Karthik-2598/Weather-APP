b62100c085b3bb7183ccdbce49826accd734ec03
KARRA SAI KARTHIK <Karrasaikarthik2001@gmail.com>
2024-08-27 20:55:32 +0530

    new build modified vite.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/karthik_weatherapp/",
+  base:"/Weather-APP/",
   build:{
     outDir: "docs",
   }
